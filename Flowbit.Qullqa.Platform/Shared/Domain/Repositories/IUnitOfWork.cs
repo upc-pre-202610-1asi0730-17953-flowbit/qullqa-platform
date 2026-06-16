1311901f404e2b2ebf3d1e242096d280ef964cc1
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-16 08:12:47 -0500

    feat(shared): add IUnitOfWork.

---
+++ Flowbit.Qullqa.Platform/Shared/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace Flowbit.Qullqa.Platform.Shared.Domain.Repositories;
+
+/// <summary>
+///     Unit of work interface for all repositories
+/// </summary>
+public interface IUnitOfWork
+{
+    /// <summary>
+    ///     Save changes to the repository
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <returns></returns>
+    Task CompleteAsync(CancellationToken cancellationToken = default);
+}
