745f5eb60dcb3600e3ca39a492eeed22cfc707d4
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-16 08:47:52 -0500

    feat(shared): add UnitOfWork.

---
+++ Flowbit.Qullqa.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Flowbit.Qullqa.Platform.Shared.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+
+
+namespace Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+
+/// <summary>
+///     Unit of work for the application.
+/// </summary>
+/// <remarks>
+///     This class is used to save changes to the database context.
+///     It implements the IUnitOfWork interface.
+/// </remarks>
+/// <param name="context">
+///     The database context for the application
+/// </param>
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    // inheritedDoc
+    public async Task CompleteAsync(CancellationToken cancellationToken = default)
+    {
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
