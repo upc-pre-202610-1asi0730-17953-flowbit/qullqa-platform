0e043359547b0d62093375c8ec3e3cf41b55ddaf
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:39:52 -0500

    feat(suppliers): update CreatePurchaseOrderResource

--- Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Resources/CreatePurchaseOrderResource.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Resources/CreatePurchaseOrderResource.cs
@@ -1,4 +1,4 @@
-namespace Qullqa.Platform.Suppliers.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest.Resources;
 
 public record CreatePurchaseOrderResource(int BusinessId, int SupplierId, string SupplierName,
     DateTimeOffset? ExpectedDate, string Description, string Currency = "PEN");
