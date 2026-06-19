b6ae9e7274734a776bb8de063b1ecf40f9d5faf8
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:59:09 -0500

    feat(shared): update ModelBuilderExtensions

--- Flowbit.Qullqa.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ Flowbit.Qullqa.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
+using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
 
-namespace Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
+namespace Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
 
 public static class ModelBuilderExtensions
 {
