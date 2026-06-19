a8f9a3d5f160796f2b73e82959259fd7a3da1b20
Alemarr2 <alejandroasmat2@gmail.com>
2026-06-19 12:45:57 -0500

    feat(alerts): implement alert repository persistence

--- Flowbit.Qullqa.Platform/Alerts/Infrastructure/Persistence/EntityFrameworkCore/Repositories/AlertRepository.cs
+++ Flowbit.Qullqa.Platform/Alerts/Infrastructure/Persistence/EntityFrameworkCore/Repositories/AlertRepository.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Qullqa.Platform.Alerts.Domain.Model.Aggregates;
-using Qullqa.Platform.Alerts.Domain.Model.Enums;
-using Qullqa.Platform.Alerts.Domain.Repositories;
-using Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
-using Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using Flowbit.Qullqa.Platform.Alerts.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Alerts.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Alerts.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
 
-namespace Qullqa.Platform.Alerts.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+namespace Flowbit.Qullqa.Platform.Alerts.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
 
 public class AlertRepository(AppDbContext context) : BaseRepository<Alert>(context), IAlertRepository
 {
