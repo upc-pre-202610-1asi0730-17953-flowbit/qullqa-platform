f0e7b2dbc63a376fa6b9fdbe183ffefded65064a
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:39:53 -0500

    feat(suppliers): update PurchaseOrderDetailResource

--- Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Resources/PurchaseOrderDetailResource.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Resources/PurchaseOrderDetailResource.cs
@@ -1,6 +1,6 @@
-using Qullqa.Platform.Suppliers.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Enums;
 
-namespace Qullqa.Platform.Suppliers.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest.Resources;
 
 public record PurchaseOrderDetailResource(int Id, int PurchaseId, int ProductId, string ProductName,
     int Quantity, decimal UnitPrice, decimal Discount, decimal LineTotal,
