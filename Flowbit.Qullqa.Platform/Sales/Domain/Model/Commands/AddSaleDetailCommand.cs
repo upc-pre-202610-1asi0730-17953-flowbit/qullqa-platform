d69cf8bd79b6a03f3a8a6abc80f3e8bb1233c086
adriana832 <u202018427@upc.edu.pe>
2026-06-19 12:18:04 -0500

    refactor(sales): update AddSaleDetail command

--- Flowbit.Qullqa.Platform/Sales/Domain/Model/Commands/AddSaleDetailCommand.cs
+++ Flowbit.Qullqa.Platform/Sales/Domain/Model/Commands/AddSaleDetailCommand.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Sales.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Sales.Domain.Model.Commands;
 
 public record AddSaleDetailCommand(int SaleId, int ProductId, int Quantity, decimal UnitPrice, decimal Discount);
