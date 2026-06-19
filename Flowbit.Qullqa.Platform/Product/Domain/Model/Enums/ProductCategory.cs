d4f9e52bbc0d1fdc21f34b19755492e01093ffd8
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:53:24 -0500

    feat(product): update ProductCategory enum

--- Flowbit.Qullqa.Platform/Product/Domain/Model/Enums/ProductCategory.cs
+++ Flowbit.Qullqa.Platform/Product/Domain/Model/Enums/ProductCategory.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Product.Domain.Model.Enums;
+namespace Flowbit.Qullqa.Platform.Product.Domain.Model.Enums;
 
 public enum ProductCategory { Dairy, Grains, Oils, Beverages, Cleaning, Medicine, Other }
