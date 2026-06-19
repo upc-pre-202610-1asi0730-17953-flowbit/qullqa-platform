85c300d47da1b3ff8727e7d2b8eafdafceaa8f21
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:53:24 -0500

    feat(product): update UpdateProductCommand

--- Flowbit.Qullqa.Platform/Product/Domain/Model/Commands/UpdateProductCommand.cs
+++ Flowbit.Qullqa.Platform/Product/Domain/Model/Commands/UpdateProductCommand.cs
@@ -1,5 +1,5 @@
-using Qullqa.Platform.Product.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Product.Domain.Model.Enums;
 
-namespace Qullqa.Platform.Product.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Product.Domain.Model.Commands;
 
 public record UpdateProductCommand(int Id, string Name, string Description, ProductCategory Category, decimal BasePrice, ProductStatus Status);
