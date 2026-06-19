8639f4772cbbc178de6d76df49a666b98da5b054
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:53:27 -0500

    feat(product): update InventoryItemResource

--- Flowbit.Qullqa.Platform/Product/Interfaces/Rest/Resources/InventoryItemResource.cs
+++ Flowbit.Qullqa.Platform/Product/Interfaces/Rest/Resources/InventoryItemResource.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Product.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Product.Interfaces.Rest.Resources;
 
 public record InventoryItemResource(int Id, int ProductId, int BusinessId, int WarehouseId, int StockUnit, int MinimumStock);
