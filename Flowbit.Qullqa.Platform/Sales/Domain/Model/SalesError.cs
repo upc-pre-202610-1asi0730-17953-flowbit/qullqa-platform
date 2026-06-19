deedbec0ba25a1946b9992608f069f87c9679fed
adriana832 <u202018427@upc.edu.pe>
2026-06-19 12:18:06 -0500

    refactor(sales): update SalesError definitions

--- Flowbit.Qullqa.Platform/Sales/Domain/Model/SalesError.cs
+++ Flowbit.Qullqa.Platform/Sales/Domain/Model/SalesError.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Sales.Domain.Model;
+namespace Flowbit.Qullqa.Platform.Sales.Domain.Model;
 
 public enum SalesError { None, SaleNotFound, CustomerNotFound, SaleAlreadyClosed, DatabaseError, InternalServerError }
