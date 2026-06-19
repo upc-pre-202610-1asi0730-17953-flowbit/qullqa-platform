3c1d671fd0022c938d5e705b040a4d60b0e21bb7
adriana832 <u202018427@upc.edu.pe>
2026-06-19 12:18:11 -0500

    refactor(sales): update Sale resource

--- Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Resources/SaleResource.cs
+++ Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Resources/SaleResource.cs
@@ -1,6 +1,6 @@
-using Qullqa.Platform.Sales.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Enums;
 
-namespace Qullqa.Platform.Sales.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Resources;
 
 public record SaleResource(int Id, int BusinessId, int? CustomerId, SaleStatus Status, decimal TotalAmount,
     PaymentMethod? PaymentMethod, DateTimeOffset Date, string Description, string Currency,
