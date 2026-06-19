c6260682ee3da950ff0181489f1e8e61a915ddea
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:39:50 -0500

    feat(suppliers): update PurchaseOrderStatus enum

--- Flowbit.Qullqa.Platform/Suppliers/Domain/Model/Enums/PurchaseOrderStatus.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Domain/Model/Enums/PurchaseOrderStatus.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Suppliers.Domain.Model.Enums;
+namespace Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Enums;
 
 public enum PurchaseOrderStatus { Generated, Received, Delayed, Cancelled }
