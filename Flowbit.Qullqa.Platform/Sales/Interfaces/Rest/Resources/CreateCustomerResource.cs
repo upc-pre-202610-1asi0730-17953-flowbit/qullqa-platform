cc9945797c28affffbc829fc853ae5a422466366
adriana832 <u202018427@upc.edu.pe>
2026-06-19 12:18:10 -0500

    refactor(sales): update CreateCustomer resource

--- Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Resources/CreateCustomerResource.cs
+++ Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Resources/CreateCustomerResource.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Sales.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Resources;
 
 public record CreateCustomerResource(int BusinessId, string FullName, string DocumentNumber, string PhoneNumber);
