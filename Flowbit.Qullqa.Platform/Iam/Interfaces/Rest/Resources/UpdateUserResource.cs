3f676ce29e852ad91f845ded650a5ee398832439
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:45:55 -0500

    feat(iam): update UpdateUserResource

--- Flowbit.Qullqa.Platform/Iam/Interfaces/Rest/Resources/UpdateUserResource.cs
+++ Flowbit.Qullqa.Platform/Iam/Interfaces/Rest/Resources/UpdateUserResource.cs
@@ -1,5 +1,5 @@
-using Qullqa.Platform.Iam.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Iam.Domain.Model.Enums;
 
-namespace Qullqa.Platform.Iam.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Iam.Interfaces.Rest.Resources;
 
 public record UpdateUserResource(string FirstName, string LastName, UserStatus Status, int? RoleId, int? BusinessId);
