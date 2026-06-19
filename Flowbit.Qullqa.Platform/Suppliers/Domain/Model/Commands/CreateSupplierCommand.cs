05e21b464b0a56b5c15bc05c1eb6da2fd6c95dfa
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:39:49 -0500

    feat(suppliers): update CreateSupplierCommand

--- Flowbit.Qullqa.Platform/Suppliers/Domain/Model/Commands/CreateSupplierCommand.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Domain/Model/Commands/CreateSupplierCommand.cs
@@ -1,6 +1,6 @@
-using Qullqa.Platform.Suppliers.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Enums;
 
-namespace Qullqa.Platform.Suppliers.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Commands;
 
 public record CreateSupplierCommand(int BusinessId, string Name, string LastName, string Ruc, string Email,
     string Phone, string Address, string ContactPerson, SupplierCategory Category);
