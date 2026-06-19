b9d6fc6afbf32b324456f7dfe245c393e98a4e01
Alemarr2 <alejandroasmat2@gmail.com>
2026-06-19 12:45:16 -0500

    feat(alerts): update create alert command

--- Flowbit.Qullqa.Platform/Alerts/Domain/Model/Commands/CreateAlertCommand.cs
+++ Flowbit.Qullqa.Platform/Alerts/Domain/Model/Commands/CreateAlertCommand.cs
@@ -1,6 +1,6 @@
-using Qullqa.Platform.Alerts.Domain.Model.Enums;
+using Flowbit.Qullqa.Platform.Alerts.Domain.Model.Enums;
 
-namespace Qullqa.Platform.Alerts.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Alerts.Domain.Model.Commands;
 
 public record CreateAlertCommand(int BusinessId, int ProductId, string ProductName, AlertType Type,
     AlertSeverity Severity, string Message, int? BatchId, int? CurrentStock, int? MinStock, int? DaysToExpiry);
