e7c0fd5aef62026c059ead2210c1930b0db35a33
Alemarr2 <alejandroasmat2@gmail.com>
2026-06-19 12:45:13 -0500

    feat(alerts): update alerts domain errors

--- Flowbit.Qullqa.Platform/Alerts/Domain/Model/AlertsError.cs
+++ Flowbit.Qullqa.Platform/Alerts/Domain/Model/AlertsError.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Alerts.Domain.Model;
+namespace Flowbit.Qullqa.Platform.Alerts.Domain.Model;
 
 public enum AlertsError { None, AlertNotFound, DatabaseError, InternalServerError }
