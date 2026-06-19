3c73b4635efc58a044045df470278b4ec596df2d
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-19 10:45:49 -0500

    feat(iam): update CreateBusinessCommand

--- Flowbit.Qullqa.Platform/Iam/Domain/Model/Commands/CreateBusinessCommand.cs
+++ Flowbit.Qullqa.Platform/Iam/Domain/Model/Commands/CreateBusinessCommand.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Iam.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Iam.Domain.Model.Commands;
 
 public record CreateBusinessCommand(string Name, string Ruc, string Email, string Phone, string Address);
