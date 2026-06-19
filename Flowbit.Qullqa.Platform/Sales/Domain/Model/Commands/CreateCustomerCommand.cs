c801f9132d1cfbeb574fa099a526804ad7962993
adriana832 <u202018427@upc.edu.pe>
2026-06-19 12:18:04 -0500

    refactor(sales): update CreateCustomer command

--- Flowbit.Qullqa.Platform/Sales/Domain/Model/Commands/CreateCustomerCommand.cs
+++ Flowbit.Qullqa.Platform/Sales/Domain/Model/Commands/CreateCustomerCommand.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.Sales.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Sales.Domain.Model.Commands;
 
 public record CreateCustomerCommand(int BusinessId, string FullName, string DocumentNumber, string PhoneNumber);
