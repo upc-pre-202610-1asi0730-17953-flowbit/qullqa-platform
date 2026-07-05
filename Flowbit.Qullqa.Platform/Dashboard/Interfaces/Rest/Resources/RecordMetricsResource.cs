fbe7cd65cc881506023f4653c157be79ad9c466b
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-07-05 16:34:47 -0500

    feature(dashboard): update Dashboard by removing legacy metrics-snapshot files for Dashboard

--- Flowbit.Qullqa.Platform/Dashboard/Interfaces/Rest/Resources/RecordMetricsResource.cs
+++
@@ -1,3 +0,0 @@
-namespace Flowbit.Qullqa.Platform.Dashboard.Interfaces.Rest.Resources;
-
-public record RecordMetricsResource(int BusinessId, int TotalProducts, int TotalSales, decimal TotalRevenue, int LowStockCount);
