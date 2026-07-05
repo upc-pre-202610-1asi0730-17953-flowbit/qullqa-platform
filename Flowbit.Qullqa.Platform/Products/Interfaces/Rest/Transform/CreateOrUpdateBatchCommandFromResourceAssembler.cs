7b09cc6c3d8e39097796c32e8afdd3c0790b564b
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:30:07 -0500

    feat(products): update CreateOrUpdateBatchCommandFromResourceAssembler.cs.

--- Flowbit.Qullqa.Platform/Products/Interfaces/Rest/Transform/CreateOrUpdateBatchCommandFromResourceAssembler.cs
+++ Flowbit.Qullqa.Platform/Products/Interfaces/Rest/Transform/CreateOrUpdateBatchCommandFromResourceAssembler.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Products.Domain.Model.Commands;
-using Qullqa.Platform.v2.Products.Interfaces.Rest.Resources;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Commands;
+using Flowbit.Qullqa.Platform.Products.Interfaces.Rest.Resources;
 
-namespace Qullqa.Platform.v2.Products.Interfaces.Rest.Transform;
+namespace Flowbit.Qullqa.Platform.Products.Interfaces.Rest.Transform;
 
 public static class CreateOrUpdateBatchCommandFromResourceAssembler
 {
