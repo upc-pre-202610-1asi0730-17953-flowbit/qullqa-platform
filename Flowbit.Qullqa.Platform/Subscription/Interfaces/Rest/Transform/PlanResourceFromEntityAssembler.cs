b72f8144f6c58db0c977e5d9becd20b0d070984b
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:44:08 -0500

    feat(subscription): add PlanResourceFromEntityAssembler.cs.

--- Flowbit.Qullqa.Platform/Subscription/Interfaces/Rest/Transform/PlanResourceFromEntityAssembler.cs
+++ Flowbit.Qullqa.Platform/Subscription/Interfaces/Rest/Transform/PlanResourceFromEntityAssembler.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Subscription.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Subscription.Interfaces.Rest.Resources;
+using Flowbit.Qullqa.Platform.Subscription.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Subscription.Interfaces.Rest.Resources;
 
-namespace Qullqa.Platform.v2.Subscription.Interfaces.Rest.Transform;
+namespace Flowbit.Qullqa.Platform.Subscription.Interfaces.Rest.Transform;
 
 public static class PlanResourceFromEntityAssembler
 {
