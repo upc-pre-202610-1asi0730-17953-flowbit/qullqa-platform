fccb502ede071302e526693a52acf346d7ee28ff
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:29:55 -0500

    feat(products): update IInventoryQueryService.cs.

--- Flowbit.Qullqa.Platform/Products/Application/QueryServices/IInventoryQueryService.cs
+++ Flowbit.Qullqa.Platform/Products/Application/QueryServices/IInventoryQueryService.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Products.Domain.Model.Entities;
-using Qullqa.Platform.v2.Products.Domain.Model.Queries;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Entities;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Queries;
 
-namespace Qullqa.Platform.v2.Products.Application.QueryServices;
+namespace Flowbit.Qullqa.Platform.Products.Application.QueryServices;
 
 public interface IInventoryQueryService
 {
