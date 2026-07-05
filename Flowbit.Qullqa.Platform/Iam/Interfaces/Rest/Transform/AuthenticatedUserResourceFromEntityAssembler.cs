827222a0a759e82b8b93842b250ea3e095839d03
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-07-05 16:53:34 -0500

    feature(migrations): update AuthenticatedUserResourceFromEntityAssembler for Iam

--- Flowbit.Qullqa.Platform/Iam/Interfaces/Rest/Transform/AuthenticatedUserResourceFromEntityAssembler.cs
+++ Flowbit.Qullqa.Platform/Iam/Interfaces/Rest/Transform/AuthenticatedUserResourceFromEntityAssembler.cs
@@ -6,5 +6,8 @@ namespace Flowbit.Qullqa.Platform.Iam.Interfaces.Rest.Transform;
 public static class AuthenticatedUserResourceFromEntityAssembler
 {
     public static AuthenticatedUserResource ToResourceFromEntity(User user, string token)
-        => new(user.Id, user.Email, user.FirstName, user.LastName, token);
+    {
+        return new AuthenticatedUserResource(user.Id, user.Email, user.Name, user.LastName, user.BusinessId,
+            user.RoleId, user.Status, user.Phone, token);
+    }
 }
