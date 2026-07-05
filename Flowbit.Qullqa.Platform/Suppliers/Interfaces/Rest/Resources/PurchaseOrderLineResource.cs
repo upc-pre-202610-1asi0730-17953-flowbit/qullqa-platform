984fd019868378bc217897eb0debf2db78ae90ba
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:54:14 -0500

    feat(suppliers): update PurchaseOrderLineResource.cs.

--- Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Resources/PurchaseOrderLineResource.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Resources/PurchaseOrderLineResource.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.v2.Suppliers.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest.Resources;
 
 public record PurchaseOrderLineResource(int ProductId, int Quantity, decimal UnitPrice, decimal Discount);
