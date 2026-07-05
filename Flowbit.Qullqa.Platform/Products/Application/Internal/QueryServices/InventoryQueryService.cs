6f5090e7847a222b13cb74ca652afb60555cc174
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:29:54 -0500

    feat(products): update InventoryQueryService.cs.

--- Flowbit.Qullqa.Platform/Products/Application/Internal/QueryServices/InventoryQueryService.cs
+++ Flowbit.Qullqa.Platform/Products/Application/Internal/QueryServices/InventoryQueryService.cs
@@ -1,9 +1,9 @@
-using Qullqa.Platform.v2.Products.Application.QueryServices;
-using Qullqa.Platform.v2.Products.Domain.Model.Entities;
-using Qullqa.Platform.v2.Products.Domain.Model.Queries;
-using Qullqa.Platform.v2.Products.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Products.Application.QueryServices;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Entities;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Queries;
+using Flowbit.Qullqa.Platform.Products.Domain.Repositories;
 
-namespace Qullqa.Platform.v2.Products.Application.Internal.QueryServices;
+namespace Flowbit.Qullqa.Platform.Products.Application.Internal.QueryServices;
 
 public class InventoryQueryService(IInventoryItemRepository inventoryItemRepository) : IInventoryQueryService
 {
