8ace01b9bba67795c90e6c0846003672c4ec44a4
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:29:58 -0500

    feat(products): update InventoryItem.cs.

--- Flowbit.Qullqa.Platform/Products/Domain/Model/Entities/InventoryItem.cs
+++ Flowbit.Qullqa.Platform/Products/Domain/Model/Entities/InventoryItem.cs
@@ -1,6 +1,6 @@
-using Qullqa.Platform.v2.Shared.Domain.Model.Services;
+using Flowbit.Qullqa.Platform.Shared.Domain.Model.Services;
 
-namespace Qullqa.Platform.v2.Products.Domain.Model.Entities;
+namespace Flowbit.Qullqa.Platform.Products.Domain.Model.Entities;
 
 /// <summary>
 ///     Current stock of one product at one warehouse. Modeled as a real N:M
