cfd0edb0291b59fa99fa1731b8bc546acd48bed6
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:34:09 -0500

    feat(sales): update SaleResourceFromEntityAssembler.cs.

--- Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Transform/SaleResourceFromEntityAssembler.cs
+++ Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Transform/SaleResourceFromEntityAssembler.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Sales.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Sales.Interfaces.Rest.Resources;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Resources;
 
-namespace Qullqa.Platform.v2.Sales.Interfaces.Rest.Transform;
+namespace Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Transform;
 
 public static class SaleResourceFromEntityAssembler
 {
