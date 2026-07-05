def70d7929c96fdabacebb59eba7798a90343bcb
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:30:08 -0500

    feat(products): update ProductActionResultAssembler.cs.

--- Flowbit.Qullqa.Platform/Products/Interfaces/Rest/Transform/ProductActionResultAssembler.cs
+++ Flowbit.Qullqa.Platform/Products/Interfaces/Rest/Transform/ProductActionResultAssembler.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Qullqa.Platform.v2.Products.Domain.Model.Errors;
-using Qullqa.Platform.v2.Shared.Application.Model;
-using Qullqa.Platform.v2.Shared.Interfaces.Rest.ProblemDetails;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Errors;
+using Flowbit.Qullqa.Platform.Shared.Application.Model;
+using Flowbit.Qullqa.Platform.Shared.Interfaces.Rest.ProblemDetails;
 
-namespace Qullqa.Platform.v2.Products.Interfaces.Rest.Transform;
+namespace Flowbit.Qullqa.Platform.Products.Interfaces.Rest.Transform;
 
 /// <summary>
 ///     Translates a Result/Result&lt;T&gt; into an IActionResult, mapping
