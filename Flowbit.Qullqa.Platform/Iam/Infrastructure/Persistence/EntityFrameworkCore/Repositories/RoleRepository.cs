5fce2301bf330a80bca5007258d9b68070fd04d7
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-07-05 16:52:44 -0500

    feature(migrations): update RoleRepository EF Core implementation for Iam

--- Flowbit.Qullqa.Platform/Iam/Infrastructure/Persistence/EntityFrameworkCore/Repositories/RoleRepository.cs
+++ Flowbit.Qullqa.Platform/Iam/Infrastructure/Persistence/EntityFrameworkCore/Repositories/RoleRepository.cs
@@ -1,5 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using Flowbit.Qullqa.Platform.Iam.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Iam.Domain.Model.Entities;
 using Flowbit.Qullqa.Platform.Iam.Domain.Repositories;
 using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
@@ -8,6 +7,4 @@ namespace Flowbit.Qullqa.Platform.Iam.Infrastructure.Persistence.EntityFramework
 
 public class RoleRepository(AppDbContext context) : BaseRepository<Role>(context), IRoleRepository
 {
-    public async Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken)
-        => await Context.Set<Role>().AnyAsync(r => r.Name == name, cancellationToken);
 }
