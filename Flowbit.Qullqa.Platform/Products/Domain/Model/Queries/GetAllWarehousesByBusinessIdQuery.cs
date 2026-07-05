cabd3d88f1475ec732c62ea8cc31798cab0d5fe5
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:30:00 -0500

    feat(products): update GetAllWarehousesByBusinessIdQuery.cs.

--- Flowbit.Qullqa.Platform/Products/Domain/Model/Queries/GetAllWarehousesByBusinessIdQuery.cs
+++ Flowbit.Qullqa.Platform/Products/Domain/Model/Queries/GetAllWarehousesByBusinessIdQuery.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.v2.Products.Domain.Model.Queries;
+namespace Flowbit.Qullqa.Platform.Products.Domain.Model.Queries;
 
 public record GetAllWarehousesByBusinessIdQuery(int BusinessId);
