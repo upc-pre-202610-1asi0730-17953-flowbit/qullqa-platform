53666b6a09f208aeafb41657c6547925b2b7b290
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:36:01 -0500

    feat(sales): update ICustomerQueryService.cs.

--- Flowbit.Qullqa.Platform/Sales/Application/QueryServices/ICustomerQueryService.cs
+++ Flowbit.Qullqa.Platform/Sales/Application/QueryServices/ICustomerQueryService.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Sales.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Sales.Domain.Model.Queries;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Queries;
 
-namespace Qullqa.Platform.v2.Sales.Application.QueryServices;
+namespace Flowbit.Qullqa.Platform.Sales.Application.QueryServices;
 
 public interface ICustomerQueryService
 {
