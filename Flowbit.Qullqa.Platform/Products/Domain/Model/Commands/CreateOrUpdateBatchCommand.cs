c366897abdc99f8aab4fbd20c1be2a37d9a53ba4
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:29:56 -0500

    feat(products): update CreateOrUpdateBatchCommand.cs.

--- Flowbit.Qullqa.Platform/Products/Domain/Model/Commands/CreateOrUpdateBatchCommand.cs
+++ Flowbit.Qullqa.Platform/Products/Domain/Model/Commands/CreateOrUpdateBatchCommand.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.v2.Products.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Products.Domain.Model.Commands;
 
 public record CreateOrUpdateBatchCommand(int ProductId, int BusinessId, DateOnly? Expiration, decimal PurchasePrice, int? InventoryId);
