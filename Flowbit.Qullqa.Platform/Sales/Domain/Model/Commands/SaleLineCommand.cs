1d3ad760c562bf37133b01da2dd67eb59e6eec23
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:36:02 -0500

    feat(sales): update SaleLineCommand.cs.

--- Flowbit.Qullqa.Platform/Sales/Domain/Model/Commands/SaleLineCommand.cs
+++ Flowbit.Qullqa.Platform/Sales/Domain/Model/Commands/SaleLineCommand.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.v2.Sales.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Sales.Domain.Model.Commands;
 
 public record SaleLineCommand(int ProductId, int Quantity, decimal UnitPrice, decimal Discount);
