f8425f28c0e08f873c6b6985b9177ce59c8e4fea
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:54:12 -0500

    feat(suppliers): update ISupplierRepository.cs.

--- Flowbit.Qullqa.Platform/Suppliers/Domain/Repositories/ISupplierRepository.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Domain/Repositories/ISupplierRepository.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Shared.Domain.Repositories;
-using Qullqa.Platform.v2.Suppliers.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Shared.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Aggregates;
 
-namespace Qullqa.Platform.v2.Suppliers.Domain.Repositories;
+namespace Flowbit.Qullqa.Platform.Suppliers.Domain.Repositories;
 
 public interface ISupplierRepository : IBaseRepository<Supplier>
 {
