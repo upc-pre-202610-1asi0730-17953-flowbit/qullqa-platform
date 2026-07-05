575f83382c5fee76129e4c6dbf5687a623b0f04a
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:36:05 -0500

    feat(sales): update CustomerResource.cs.

--- Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Resources/CustomerResource.cs
+++ Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Resources/CustomerResource.cs
@@ -1,4 +1,4 @@
-namespace Qullqa.Platform.v2.Sales.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Resources;
 
 public record CustomerResource(int Id, int BusinessId, string FullName, string DocumentNumber, string PhoneNumber,
     string Email, DateTimeOffset RegisteredAt);
