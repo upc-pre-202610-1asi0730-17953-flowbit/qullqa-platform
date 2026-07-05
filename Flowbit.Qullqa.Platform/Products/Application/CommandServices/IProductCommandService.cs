f8400fef2a74c963bdaa8c2daa62873768e00ce8
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:29:52 -0500

    feat(products): update IProductCommandService.cs.

--- Flowbit.Qullqa.Platform/Products/Application/CommandServices/IProductCommandService.cs
+++ Flowbit.Qullqa.Platform/Products/Application/CommandServices/IProductCommandService.cs
@@ -1,8 +1,8 @@
-using Qullqa.Platform.v2.Products.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Products.Domain.Model.Commands;
-using Qullqa.Platform.v2.Shared.Application.Model;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Commands;
+using Flowbit.Qullqa.Platform.Shared.Application.Model;
 
-namespace Qullqa.Platform.v2.Products.Application.CommandServices;
+namespace Flowbit.Qullqa.Platform.Products.Application.CommandServices;
 
 public interface IProductCommandService
 {
