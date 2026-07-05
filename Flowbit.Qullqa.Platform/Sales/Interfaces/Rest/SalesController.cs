dec950e3967a6b545b3eceae831f5bb07d11d754
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:34:08 -0500

    feat(sales): update SalesController.cs.

--- Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/SalesController.cs
+++ Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/SalesController.cs
@@ -1,17 +1,17 @@
 using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
-using Qullqa.Platform.v2.Iam.Infrastructure.Pipeline.Middleware.Attributes;
-using Qullqa.Platform.v2.Sales.Application.CommandServices;
-using Qullqa.Platform.v2.Sales.Application.QueryServices;
-using Qullqa.Platform.v2.Sales.Domain.Model.Commands;
-using Qullqa.Platform.v2.Sales.Domain.Model.Queries;
-using Qullqa.Platform.v2.Sales.Interfaces.Rest.Resources;
-using Qullqa.Platform.v2.Sales.Interfaces.Rest.Transform;
-using Qullqa.Platform.v2.Shared.Application;
-using Qullqa.Platform.v2.Shared.Interfaces.Rest.ProblemDetails;
+using Flowbit.Qullqa.Platform.Iam.Infrastructure.Pipeline.Middleware.Attributes;
+using Flowbit.Qullqa.Platform.Sales.Application.CommandServices;
+using Flowbit.Qullqa.Platform.Sales.Application.QueryServices;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Commands;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Queries;
+using Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Resources;
+using Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Transform;
+using Flowbit.Qullqa.Platform.Shared.Application;
+using Flowbit.Qullqa.Platform.Shared.Interfaces.Rest.ProblemDetails;
 using Swashbuckle.AspNetCore.Annotations;
 
-namespace Qullqa.Platform.v2.Sales.Interfaces.Rest;
+namespace Flowbit.Qullqa.Platform.Sales.Interfaces.Rest;
 
 [Authorize]
 [ApiController]
