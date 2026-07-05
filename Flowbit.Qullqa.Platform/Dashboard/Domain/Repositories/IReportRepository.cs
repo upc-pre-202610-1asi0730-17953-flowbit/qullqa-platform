e725884e5df674f7091b3b4436939edcf4e47c25
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-07-05 16:34:50 -0500

    feature(dashboard): update IReportRepository and its EF Core implementation for Dashboard

--- Flowbit.Qullqa.Platform/Dashboard/Domain/Repositories/IReportRepository.cs
+++ Flowbit.Qullqa.Platform/Dashboard/Domain/Repositories/IReportRepository.cs
@@ -1,9 +1,9 @@
-using Flowbit.Qullqa.Platform.Dashboard.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Dashboard.Domain.Model.Entities;
 using Flowbit.Qullqa.Platform.Shared.Domain.Repositories;
 
 namespace Flowbit.Qullqa.Platform.Dashboard.Domain.Repositories;
 
 public interface IReportRepository : IBaseRepository<Report>
 {
-    Task<IEnumerable<Report>> FindByBusinessIdAsync(int businessId, CancellationToken cancellationToken);
+    Task<IEnumerable<Report>> FindAllByBusinessIdAsync(int businessId, CancellationToken cancellationToken = default);
 }
--- Flowbit.Qullqa.Platform/Dashboard/Infrastructure/Persistence/EntityFrameworkCore/Repositories/ReportRepository.cs
+++ Flowbit.Qullqa.Platform/Dashboard/Infrastructure/Persistence/EntityFrameworkCore/Repositories/ReportRepository.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Flowbit.Qullqa.Platform.Dashboard.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Dashboard.Domain.Model.Entities;
 using Flowbit.Qullqa.Platform.Dashboard.Domain.Repositories;
 using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 using Flowbit.Qullqa.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
@@ -8,6 +8,9 @@ namespace Flowbit.Qullqa.Platform.Dashboard.Infrastructure.Persistence.EntityFra
 
 public class ReportRepository(AppDbContext context) : BaseRepository<Report>(context), IReportRepository
 {
-    public async Task<IEnumerable<Report>> FindByBusinessIdAsync(int businessId, CancellationToken cancellationToken)
-        => await Context.Set<Report>().Where(r => r.BusinessId == businessId).ToListAsync(cancellationToken);
+    public async Task<IEnumerable<Report>> FindAllByBusinessIdAsync(int businessId, CancellationToken cancellationToken = default)
+    {
+        return await Context.Set<Report>().Where(report => report.BusinessId == businessId)
+            .OrderByDescending(report => report.GeneratedAt).ToListAsync(cancellationToken);
+    }
 }
