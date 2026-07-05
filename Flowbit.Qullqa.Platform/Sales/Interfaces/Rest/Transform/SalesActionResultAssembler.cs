6b89f51147458d279c43ee2ce934d7755436d42a
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:34:09 -0500

    feat(sales): update SalesActionResultAssembler.cs.

--- Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Transform/SalesActionResultAssembler.cs
+++ Flowbit.Qullqa.Platform/Sales/Interfaces/Rest/Transform/SalesActionResultAssembler.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Qullqa.Platform.v2.Sales.Domain.Model.Errors;
-using Qullqa.Platform.v2.Shared.Application.Model;
-using Qullqa.Platform.v2.Shared.Interfaces.Rest.ProblemDetails;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Errors;
+using Flowbit.Qullqa.Platform.Shared.Application.Model;
+using Flowbit.Qullqa.Platform.Shared.Interfaces.Rest.ProblemDetails;
 
-namespace Qullqa.Platform.v2.Sales.Interfaces.Rest.Transform;
+namespace Flowbit.Qullqa.Platform.Sales.Interfaces.Rest.Transform;
 
 public static class SalesActionResultAssembler
 {
