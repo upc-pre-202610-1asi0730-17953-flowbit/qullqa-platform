4b134a7677ccb30ce176cba6c2fbdd68ab076fc9
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:30:01 -0500

    feat(products): update IProductRepository.cs.

--- Flowbit.Qullqa.Platform/Products/Domain/Repositories/IProductRepository.cs
+++ Flowbit.Qullqa.Platform/Products/Domain/Repositories/IProductRepository.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Products.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Shared.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Products.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Shared.Domain.Repositories;
 
-namespace Qullqa.Platform.v2.Products.Domain.Repositories;
+namespace Flowbit.Qullqa.Platform.Products.Domain.Repositories;
 
 public interface IProductRepository : IBaseRepository<Product>
 {
