570d0e03788f80d011606e29770f41e545ff65e7
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:29:56 -0500

    feat(products): update CreateWarehouseCommand.cs.

--- Flowbit.Qullqa.Platform/Products/Domain/Model/Commands/CreateWarehouseCommand.cs
+++ Flowbit.Qullqa.Platform/Products/Domain/Model/Commands/CreateWarehouseCommand.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.v2.Products.Domain.Model.Commands;
+namespace Flowbit.Qullqa.Platform.Products.Domain.Model.Commands;
 
 public record CreateWarehouseCommand(int BusinessId, string Name, string Code, string Address, string Capacity);
