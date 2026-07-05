d0204b797ded1f7dd20c71df7136b49f4fd5dfd3
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:36:00 -0500

    feat(sales): update SaleCommandService.cs.

--- Flowbit.Qullqa.Platform/Sales/Application/Internal/CommandServices/SaleCommandService.cs
+++ Flowbit.Qullqa.Platform/Sales/Application/Internal/CommandServices/SaleCommandService.cs
@@ -1,17 +1,17 @@
 using Cortex.Mediator;
 using Microsoft.Extensions.Localization;
-using Qullqa.Platform.v2.Products.Interfaces.Acl;
-using Qullqa.Platform.v2.Sales.Application.CommandServices;
-using Qullqa.Platform.v2.Sales.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Sales.Domain.Model.Commands;
-using Qullqa.Platform.v2.Sales.Domain.Model.Errors;
-using Qullqa.Platform.v2.Sales.Domain.Model.Events;
-using Qullqa.Platform.v2.Sales.Domain.Repositories;
-using Qullqa.Platform.v2.Sales.Resources;
-using Qullqa.Platform.v2.Shared.Application.Model;
-using Qullqa.Platform.v2.Shared.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Products.Interfaces.Acl;
+using Flowbit.Qullqa.Platform.Sales.Application.CommandServices;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Commands;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Errors;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Events;
+using Flowbit.Qullqa.Platform.Sales.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Sales.Resources;
+using Flowbit.Qullqa.Platform.Shared.Application.Model;
+using Flowbit.Qullqa.Platform.Shared.Domain.Repositories;
 
-namespace Qullqa.Platform.v2.Sales.Application.Internal.CommandServices;
+namespace Flowbit.Qullqa.Platform.Sales.Application.Internal.CommandServices;
 
 public class SaleCommandService(
     ISaleRepository saleRepository,
