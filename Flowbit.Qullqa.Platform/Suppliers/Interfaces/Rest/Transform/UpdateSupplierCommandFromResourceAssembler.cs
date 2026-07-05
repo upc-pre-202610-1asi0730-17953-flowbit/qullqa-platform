cdc8af2efc8135fba07a056dc42d244165772455
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:54:17 -0500

    feat(suppliers): update UpdateSupplierCommandFromResourceAssembler.cs.

--- Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Transform/UpdateSupplierCommandFromResourceAssembler.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/Transform/UpdateSupplierCommandFromResourceAssembler.cs
@@ -1,7 +1,7 @@
-using Qullqa.Platform.v2.Suppliers.Domain.Model.Commands;
-using Qullqa.Platform.v2.Suppliers.Interfaces.Rest.Resources;
+using Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Commands;
+using Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest.Resources;
 
-namespace Qullqa.Platform.v2.Suppliers.Interfaces.Rest.Transform;
+namespace Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest.Transform;
 
 public static class UpdateSupplierCommandFromResourceAssembler
 {
