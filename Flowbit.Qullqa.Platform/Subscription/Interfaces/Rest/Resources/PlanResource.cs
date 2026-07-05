bbde0aac9dcc1df4abcb4ab7f09999143b50828a
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:44:07 -0500

    feat(subscription): add PlanResource.cs.

--- Flowbit.Qullqa.Platform/Subscription/Interfaces/Rest/Resources/PlanResource.cs
+++ Flowbit.Qullqa.Platform/Subscription/Interfaces/Rest/Resources/PlanResource.cs
@@ -1,4 +1,4 @@
-namespace Qullqa.Platform.v2.Subscription.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Subscription.Interfaces.Rest.Resources;
 
 public record PlanResource(int Id, string Name, string Description, decimal Price, string Currency, string TimeLength,
     string Status, IReadOnlyCollection<string> Features);
