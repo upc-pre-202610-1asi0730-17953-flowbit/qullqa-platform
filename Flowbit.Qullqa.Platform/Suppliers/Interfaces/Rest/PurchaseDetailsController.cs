6f6615619509e593164de444cbd708d5ba64b0b7
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:54:13 -0500

    feat(suppliers): update PurchaseDetailsController.cs.

--- Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/PurchaseDetailsController.cs
+++ Flowbit.Qullqa.Platform/Suppliers/Interfaces/Rest/PurchaseDetailsController.cs
@@ -1,12 +1,12 @@
 using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
-using Qullqa.Platform.v2.Iam.Infrastructure.Pipeline.Middleware.Attributes;
-using Qullqa.Platform.v2.Suppliers.Application.QueryServices;
-using Qullqa.Platform.v2.Suppliers.Domain.Model.Queries;
-using Qullqa.Platform.v2.Suppliers.Interfaces.Rest.Transform;
+using Flowbit.Qullqa.Platform.Iam.Infrastructure.Pipeline.Middleware.Attributes;
+using Flowbit.Qullqa.Platform.Suppliers.Application.QueryServices;
+using Flowbit.Qullqa.Platform.Suppliers.Domain.Model.Queries;
+using Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest.Transform;
 using Swashbuckle.AspNetCore.Annotations;
 
-namespace Qullqa.Platform.v2.Suppliers.Interfaces.Rest;
+namespace Flowbit.Qullqa.Platform.Suppliers.Interfaces.Rest;
 
 /// <summary>
 ///     Read-only by design, same reasoning as Sales' SaleDetailsController: a
