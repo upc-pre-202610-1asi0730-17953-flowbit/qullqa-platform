1e0c2e11db8952abe5b1afbe460be2b1d7a4933f
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:30:04 -0500

    feat(products): update BatchResource.cs.

--- Flowbit.Qullqa.Platform/Products/Interfaces/Rest/Resources/BatchResource.cs
+++ Flowbit.Qullqa.Platform/Products/Interfaces/Rest/Resources/BatchResource.cs
@@ -1,3 +1,3 @@
-namespace Qullqa.Platform.v2.Products.Interfaces.Rest.Resources;
+namespace Flowbit.Qullqa.Platform.Products.Interfaces.Rest.Resources;
 
 public record BatchResource(int Id, int ProductId, int BusinessId, DateOnly? Expiration, decimal PurchasePrice, string Status, int? InventoryId);
