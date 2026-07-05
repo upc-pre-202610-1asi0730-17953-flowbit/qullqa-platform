38d086767c52763889b50f25f4fed733db2c43dd
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-05 16:36:00 -0500

    feat(sales): update SaleQueryService.cs.

--- Flowbit.Qullqa.Platform/Sales/Application/Internal/QueryServices/SaleQueryService.cs
+++ Flowbit.Qullqa.Platform/Sales/Application/Internal/QueryServices/SaleQueryService.cs
@@ -1,9 +1,9 @@
-using Qullqa.Platform.v2.Sales.Application.QueryServices;
-using Qullqa.Platform.v2.Sales.Domain.Model.Aggregates;
-using Qullqa.Platform.v2.Sales.Domain.Model.Queries;
-using Qullqa.Platform.v2.Sales.Domain.Repositories;
+using Flowbit.Qullqa.Platform.Sales.Application.QueryServices;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Aggregates;
+using Flowbit.Qullqa.Platform.Sales.Domain.Model.Queries;
+using Flowbit.Qullqa.Platform.Sales.Domain.Repositories;
 
-namespace Qullqa.Platform.v2.Sales.Application.Internal.QueryServices;
+namespace Flowbit.Qullqa.Platform.Sales.Application.Internal.QueryServices;
 
 public class SaleQueryService(ISaleRepository saleRepository) : ISaleQueryService
 {
